0bac9640f3d01e5c1b3c29b9e4aeab39313d33cd
Abdulrahman Salau <bob.salau@gmail.com>
2020-08-16 09:14:21 +0100

    added route interface to spawn up routes.

--- main.go
+++ main.go
@@ -1,31 +1,43 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/gofiber/fiber"
 	"github.com/gofiber/logger"
 	"github.com/jinzhu/gorm"
+	Config "github.com/raene/fiberWeb/config"
 	"github.com/raene/fiberWeb/database"
 	"github.com/raene/fiberWeb/handlers/books"
 	"github.com/raene/fiberWeb/handlers/products"
 )
 
+type Routes interface {
+	SetupRoutes()
+}
+
+func spawnRoutes(m chan string, r ...Routes) {
+	for _, v := range r {
+		v.SetupRoutes()
+	}
+	m <- "Routes Setup"
+}
+
 func main() {
 	var c chan *gorm.DB = make(chan *gorm.DB)
+	var m chan string = make(chan string)
 	go database.InitDatabase(c)
 
-	db := <-c
-
-	a := &products.App{}
-	b := &books.App{}
 	app := fiber.New()
 	api := app.Group("/api", logger.New())
-	a.Router = api
-	b.Router = api
-	b.Db = db
+	config := Config.AppInit(api, c)
 
-	go b.SetupRoutes()
-	go a.SetupRoutes()
+	b := &books.Book{Config: config}
+	p := &products.Product{Config: config}
+
+	go spawnRoutes(m, b, p)
+
+	fmt.Println(<-m)
 	app.Listen(3000)
 
-	defer db.Close()
 }
